2307ad0a343a7adacf9c8c820e43026f01aad6d3
Emilio <3357168+Dellrodar@users.noreply.github.com>
2020-09-24 08:11:27 -0400

    Config changes (#8)
    * small change made to cors
    * modified script for testing

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,6 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
+const knex = require("knex")
+const knexfile = require("../knexfile.js")
 
-const env = process.env.NODE_ENV || "production"
+const env = process.env.NODE_ENV || "development";
 
-module.exports = knex(knexConfig[env]);
+module.exports = knex(knexfile[env])
--- server.js
+++ server.js
@@ -12,20 +12,9 @@ const server = express();
 
 
 server.use(helmet());
-let whitelist = ['http://localhost:8080','http://localhost:3000', 'https://secretrecipes.vercel.app/', 'https://tt720-secret-family-recipes.herokuapp.com/']
 
 server.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin
-    if (!origin) {
-        return callback(null, true);
-    }
-    if (whitelist.indexOf(origin) === -1) {
-      var message = 'The CORS policy for this origin doesn\'t allow access from the particular origin.';
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  },
+  origin: ['http://localhost:8080','http://localhost:3000', 'https://secretrecipes.vercel.app'],
   credentials: true
 }));
 
